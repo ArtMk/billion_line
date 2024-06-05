1982dd69330c308ed0e11bc8f413da0d357b4fd3
Artak Mkrtchyan <artako@artak-nexus5.fritz.box>
2024-06-05 23:39:00 +0200

    calculation of mean temperature for selected city but it's not working

--- main.c
+++ main.c
@@ -1,27 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
 
 int main(void) {
 
     FILE *file;
 
-    char vector[100];
+    char measurement[100];
+    char *city;
+    char *temperature;
     int i = 1;
+    float temps = 0;
+    float mean_temp;
 
+    // open file
     file = fopen("/Users/artako/CLionProjects/billion_line/measurements.txt", "r");
 
+    // make sure file is not empty
     if (file == NULL) {
         printf("There is nothing here you idiot\n");
         return 1;
     }
 
-    while (fgets(vector, 100, file) != NULL) {
-        // fgets(vector, 100, file);
-        // printf("%s", vector);
+    // raed the file line by line
+    while (fgets(measurement, 100, file) != NULL) {
+
+        // city selection
+        city = strtok(measurement, ";");
+        if (strcmp(city, "Rome") == 0) {
+
+            // temperature readings for selected city
+            temperature = strtok(NULL, "NULL");
+            // printf("%s\n", temperature);
+
+            // summation of temperature readings
+            temps += strtof(temperature, NULL);
+            // printf("%f\n", temps);
+        }
+
+        // loop cancellation
         i++;
     }
 
-    printf("%d\n", i-1);
+    // number of lines in input file
+    // printf("%d\n", i-1);
 
+    // mean temperature for selected city
+    mean_temp = temps/i;
+
+    printf("Rome\n");
+    printf("%f\n", mean_temp);
+
+    // 1.078057
+
+    // close file
     fclose(file);
 
     return 0;
