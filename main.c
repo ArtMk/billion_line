540bffc121db8b9d7d5aa5d101b6cc7b50e9a8ab
Artak Mkrtchyan <artako@artak-nexus5.fritz.box>
2024-06-12 14:31:57 +0200

    max and min also working for selected city

--- main.c
+++ main.c
@@ -11,8 +11,11 @@ int main(void) {
     char *temperature;
     int i = 0;
     int j = 0;
-    char selected[100] = "Hamburg";
+    char selected[100] = "Rome";
+    float T;
     float mean;
+    float min = 0;
+    float max = 0;
 
     // open file
     file = fopen("/Users/artako/CLionProjects/billion_line/measurements.txt", "r");
@@ -37,26 +40,41 @@ int main(void) {
             temperature = strtok(NULL, "NULL");
             // printf("%s\n", temperature);
 
+            // temperature as float
+            T = strtof(temperature, NULL);
+
             // summation of temperature readings
-            mean += strtof(temperature, NULL);
+            mean += T;
             // printf("%f\n", temps);
 
             // count city occurrences
             j++;
+
+            if (T < min) {
+                min = T;
+            }
+
+            if (T > max) {
+                max = T;
+            }
         }
 
         // count number of rows
         i++;
     }
 
+    printf("\n");
     printf("Total number of rows: %d\n", i);
     printf("Number of city occurrences: %d\n", j);
+    printf("\n");
     printf("Selected city: %s\n", selected);
+    printf("\n");
 
-    // mean temperature of selected city
     mean = mean / j;
-    printf("Mean temperature: %f\n", mean);
-
+    printf("Maximum temperature:  %.1f\n", max);
+    printf("Minimum temperature: %.1f\n", min);
+    printf("----------------------------\n");
+    printf("Mean temperature:     %.1f\n", mean);
     // close file
     fclose(file);
 
